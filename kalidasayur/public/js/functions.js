1b747d874a627fa66f625ac9300f8646d10fb352
Syed Mujeer Hashmi <mujeerhashmi@4csolutions.in>
2024-05-18 22:53:23 +0530

    Update functions.js

--- kalidasayur/public/js/functions.js
+++ kalidasayur/public/js/functions.js
@@ -1,7 +1,7 @@
 /* ==============================================
 	Preload
 =============================================== */
-$(window).load(function() { // makes sure the whole site is loaded
+$(window).on('load', (function() { // makes sure the whole site is loaded
 	'use strict';
 	$('.pulse ').fadeOut(); // will first fade out the loading animation
 	$('#preloader').delay(250).fadeOut('slow'); // will fade out the white DIV that covers the website.
